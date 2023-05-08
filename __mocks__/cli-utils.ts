ece0aabe91bae56dec84e7c4c0c1d0f59c0fcd49
joeldenning <joeldenning@gmail.com>
2023-05-08 16:58:05 -0600

    Fix tests

--- __mocks__/cli-utils.ts
+++ __mocks__/cli-utils.ts
@@ -1,25 +1,28 @@
 import { jest } from "@jest/globals";
+import { DeployArgs } from "../src/js-api";
 
 export const exitWithError = jest.fn();
 export const log = jest.fn();
 export const baseplateFetchMocks = {};
-export const baseplateFetch = async (url, init): Promise<any> => {
-  const mock = baseplateFetchMocks[url];
-  if (!mock) {
-    throw Error(`baseplateFetch mock not set up for url '${url}'`);
-  }
+export const createBaseplateFetch = (deployArgs: DeployArgs) => {
+  return async function baseplateFetch(url, init): Promise<any> {
+    const mock = baseplateFetchMocks[url];
+    if (!mock) {
+      throw Error(`baseplateFetch mock not set up for url '${url}'`);
+    }
 
-  delete baseplateFetchMocks[url];
+    delete baseplateFetchMocks[url];
 
-  if (mock instanceof Error) {
-    throw mock;
-  } else if (typeof mock === "number") {
-    throw Error(
-      `Baseplate API for url '${url}' responded with HTTP status ${mock}`
-    );
-  } else if (typeof mock === "object" && mock !== null) {
-    return mock;
-  } else {
-    throw Error(`Invalid baseplateFetch mock - is type '${typeof mock}'`);
-  }
+    if (mock instanceof Error) {
+      throw mock;
+    } else if (typeof mock === "number") {
+      throw Error(
+        `Baseplate API for url '${url}' responded with HTTP status ${mock}`
+      );
+    } else if (typeof mock === "object" && mock !== null) {
+      return mock;
+    } else {
+      throw Error(`Invalid baseplateFetch mock - is type '${typeof mock}'`);
+    }
+  };
 };
